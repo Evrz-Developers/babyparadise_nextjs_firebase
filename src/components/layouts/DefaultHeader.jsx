c630f0bd1425daa97d550ff90b98b56d8a5d4113
Robin K <dev.robinkurian@email.com>
2024-11-19 23:27:04 +0530

    feat: update DefaultHeader, DefaultFooter & DefaultLayout with responsive Ui. feat: use the updated DefaultLayout in root page add Home Component for HomePage

--- src/components/layouts/DefaultHeader.jsx
+++ src/components/layouts/DefaultHeader.jsx
@@ -1,12 +1,14 @@
-import Navbar from '@/components/common/Navbar';
+import React from 'react';
+import MainNavbar from '@/components/common/MainNavbar';
 
 const DefaultHeader = () => {
   return (
-    <>
-      <header className="bg-gray-800 text-white text-center">
-        <Navbar title="Baby Paradise" />
-      </header>
-    </>
+    <header className="bg-primary-background text-primary text-center border-b flex flex-col mx-auto w-full">
+      <div className="w-full h-5s bg-purple-400">
+        {/* TODO: Change title once fixed */}
+        <MainNavbar title="SHOP" />
+      </div>
+    </header>
   );
 };
 
---
+++ src/components/shop/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@nextui-org/button';
+
+const Home = () => {
+    return (
+        <>
+            {/* TODO: Change content */}
+            <h1 className="text-2xl font-bold">Welcome to Baby Paradise</h1>
+            <Button>Press me</Button>
+        </>
+    );
+};
+
+export default Home;

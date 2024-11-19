83d1ab4ba622253bef4f3c54d388eb0368199e16
Robin K <dev.robinkurian@email.com>
2024-11-19 17:59:03 +0530

    fix home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,27 @@
-import Dashboard from "@/components/shop/Dashboard";
+import ContentWrapper from "@/components/layouts/ContentWrapper";
+import DefaultFooter from "@/components/layouts/DefaultFooter";
+import DefaultHeader from "@/components/layouts/DefaultHeader";
 import BlurFade from "@/components/ui/blur-fade";
 
 export default function Home() {
   return (
-    <div className="">
-      <section id="header">
-        <BlurFade delay={0.25} inView>
-          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-            Baby Paradise
-          </h2>
-        </BlurFade>
-        <BlurFade delay={0.25 * 2} inView>
-          <span className="text-lg text-pretty tracking-tighter sm:text-xl xl:text-xl/none">
-            A one stop shop for all your kids&apos; needs.
-          </span>
-          <Dashboard />
-        </BlurFade>
-
-      </section>
-    </div>
+    <>
+      <DefaultHeader />
+      <ContentWrapper>
+        <section id="header" className="flex flex-col items-center justify-center">
+          <BlurFade delay={0.25} inView>
+            <h2 className="text-2xl font-bold tracking-tighter xl:text-4xl text-center">
+              Baby Paradise
+            </h2>
+          </BlurFade>
+          <BlurFade delay={0.25 * 2} inView>
+            <span className="text-lg text-pretty tracking-tighter xl:text-xl text-center">
+              A one stop shop for all your kids&apos; needs.
+            </span>
+          </BlurFade>
+        </section>
+      </ContentWrapper>
+      <DefaultFooter />
+    </>
   );
 }

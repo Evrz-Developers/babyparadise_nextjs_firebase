d45438b073efd5f71a38e56d387e88decec29570
Robin K <dev.robinkurian@email.com>
2024-11-19 23:29:27 +0530

    Update: replace button with nextUi Button, add MainNavbar, fix responsiveness, add search bar,user icon and hamburger dropdown in navbar add:Content and Page wrappers for reuse and consistent layout

---
+++ src/components/common/MainNavbar.jsx
@@ -0,0 +1,115 @@
+"use client";
+import React from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar } from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.jsx";
+import { SearchIcon } from "./SearchIcon.jsx";
+import Image from "next/image.js";
+
+const MainNavbar = ({ title }) => {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const menuItems = [
+        "Profile",
+        "Dashboard",
+        "Activity",
+        "Help & Feedback",
+        "Log Out",
+    ];
+
+    return (
+        <Navbar onMenuOpenChange={setIsMenuOpen} maxWidth="2xl" className="px-0" classNames={{
+            wrapper: "justify-start",
+            content: "data-[justify=start]:!flex-grow-0 md:flex-grow"
+        }}>
+            {/* Logo and title */}
+            <NavbarContent className="gap-2 flex-grow-0">
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden" />
+                <NavbarBrand>
+                    {/* TODO: Change logo */}
+                    <AcmeLogo />
+                    <p className="font-bold text-inherit">{title}</p>
+                </NavbarBrand>
+            </NavbarContent>
+
+            {/* TODO: Move menu items to second row */}
+            {/* <NavbarContent className="hidden sm:flex gap-2" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Features
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="#" aria-current="page">
+                        Customers
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Integrations
+                    </Link>
+                </NavbarItem>
+            </NavbarContent> */}
+
+            {/* Search */}
+            <NavbarContent as="div" className="items-center gap-2" justify="center">
+                <Input
+                    classNames={{
+                        base: "max-w-full sm:max-w-[20rem] h-10 pr-1",
+                        mainWrapper: "h-full",
+                        input: "text-small",
+                        inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+                    }}
+                    placeholder="Type to search..."
+                    size="sm"
+                    startContent={<SearchIcon size={18} />}
+                    type="search"
+                />
+            </NavbarContent>
+
+            {/* Profile */}
+            <Dropdown placement="bottom-end">
+                <DropdownTrigger>
+                    <Image
+                        alt="Profile"
+                        width={32}
+                        height={32}
+                        src="/images/logo.png"
+                    />
+                </DropdownTrigger>
+                <DropdownMenu aria-label="Profile Actions" variant="flat">
+                    <DropdownItem key="profile" className="h-14 gap-2">
+                        <p className="font-semibold">Signed in as</p>
+                        <p className="font-semibold">zoey@example.com</p>
+                    </DropdownItem>
+                    <DropdownItem key="settings">My Settings</DropdownItem>
+                    <DropdownItem key="team_settings">Team Settings</DropdownItem>
+                    <DropdownItem key="analytics">Analytics</DropdownItem>
+                    <DropdownItem key="system">System</DropdownItem>
+                    <DropdownItem key="configurations">Configurations</DropdownItem>
+                    <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+                    <DropdownItem key="logout" color="danger">
+                        Log Out
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+
+            {/* Mobile menu items */}
+            <NavbarMenu>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <Link
+                            color={index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"}
+                            className="w-full"
+                            href="#"
+                            size="lg"
+                        >
+                            {item}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
+    );
+};
+
+export default MainNavbar;

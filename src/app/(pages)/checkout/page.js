4beb3081365d851a8b04e03fa4bfe8440b19debb
Robin K <dev.robinkurian@email.com>
2024-11-21 04:12:17 +0530

    add login, register,cart, product and checkout pages

---
+++ src/app/(pages)/checkout/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import DummyCard from '@/components/common/DummyCard';
+
+const checkoutPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center pt-32">
+            <DummyCard
+                gradientColor="#2634"
+                title="🚧"
+                description="We'll be back soon."
+            />
+        </div>
+    )
+}
+
+export default checkoutPage

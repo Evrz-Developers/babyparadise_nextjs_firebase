3d9ce20607c28d705007b781d16425b96f5183a2
Robin K <dev.robinkurian@email.com>
2024-11-27 22:38:48 +0530

    save the theme state to localStorage and retrieve it if it exists

--- src/components/common/ThemeSwitcher.jsx
+++ src/components/common/ThemeSwitcher.jsx
@@ -2,10 +2,14 @@ import { useState, useEffect } from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
 const ThemeSwitcher = () => {
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState(() => {
+        const savedTheme = localStorage.getItem('theme');
+        return savedTheme ? savedTheme : 'light';
+    });
 
     useEffect(() => {
         document.documentElement.classList.toggle('dark', theme === 'dark');
+        localStorage.setItem('theme', theme);
     }, [theme]);
 
     const toggleTheme = () => {

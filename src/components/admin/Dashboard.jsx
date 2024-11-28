24458ec6d7a8983e84e5bbbdee5f47bc5fa6ae1e
Robin K <dev.robinkurian@email.com>
2024-11-28 23:34:03 +0530

    add dashboard component - layout

---
+++ src/components/admin/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DummyCard from '@/components/common/DummyCard';
+
+const Dashboard = () => {
+    return (
+        <div className="flex flex-col items-center justify-center pt-32">
+            <DummyCard
+                gradientColor="#2634"
+                title="🚧 Dashboard"
+            />
+        </div>
+    )
+}
+
+export default Dashboard

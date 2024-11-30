39c1a7c260a5b26376069b5529583a507a6d3374
Robin K <dev.robinkurian@email.com>
2024-11-30 06:46:52 +0530

    fetch products from serverside

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,36 @@
 import Home from "@/components/shop/Home";
 
-export default function HomePage() {
+async function fetchProducts(queryParams = "") {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products${queryParams}`, {
+      cache: "no-store", 
+    });
+
+    if (!res.ok) throw new Error("Failed to fetch products");
+
+    const data = await res.json();
+    console.log(data);
+    return data?.data || [];
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return [];
+  }
+}
+// TODO: Remove this function after testing
+async function fetchDemoProducts() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/data/products.json`);
+  const data = await response.json();
+  return data;
+}
+
+export default async function Page() {
+  const queryParams = ""; // Optionally add category query like `?category=toy`
+  // const initialProducts = await fetchProducts(queryParams); // Fetch all products on the server side
+  const initialProducts = await fetchDemoProducts(); // Fetch all products on the server side
+
   return (
-    <Home />
+    <div>
+      <Home initialProducts={initialProducts} />
+    </div>
   );
 }

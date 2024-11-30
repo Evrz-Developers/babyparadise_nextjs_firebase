37abca32774d6251fe969520844323dc85893c3f
Robin K <dev.robinkurian@email.com>
2024-11-30 06:43:03 +0530

    refactor api folder paths

---
+++ src/utilities/api/product.api.js
@@ -0,0 +1,20 @@
+const getProducts = async (category = null) => {
+    try {
+      const url = new URL(`/api/products`);
+      if (category) url.searchParams.append("category", category); // Optionally filter by category
+  
+      const response = await fetch(url);
+      const data = await response.json();
+      return { data: data.data };
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      throw error;
+    }
+  };
+  
+  const PRODUCT_API = {
+    getProducts,
+  };
+  
+  export default PRODUCT_API;
+  

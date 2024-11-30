dd98309b579010c293b9c3219973a576a8c490df
Robin K <dev.robinkurian@email.com>
2024-11-30 06:47:14 +0530

    add product card

--- src/components/layouts/ProductCard.jsx
+++ src/components/layouts/ProductCard.jsx
@@ -2,13 +2,15 @@ import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 
 const ProductCard = ({ product }) => {
   return (
-    <Card className="pb-4">
+    <Card className="pb-2 bg-white" isPressable onPress={() => console.log("item pressed")}>
       {/* <CardBody className="overflow-visible py-2"> */}
         <Image
           alt="Card background"
-          className="object-cover rounded-xl mix-blend-multiply bg-transparent"
-          src="/images/shop/bp1.jpg"
-          width={270}
+          className="object-center rounded-xl mix-blend-soft-light bg-white"
+          src="/images/shop/test.jpg"
+          layout="fixed" // Ensures the image maintains its aspect ratio
+          width={200} // Fixed width
+          height={200} // Fixed height
         />
       {/* </CardBody> */}
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">

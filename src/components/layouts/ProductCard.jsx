89e67645bd061c8b35ac727f19fcbfb34402c945
Robin K <dev.robinkurian@email.com>
2024-11-30 08:40:33 +0530

    fix image display style issue

--- src/components/layouts/ProductCard.jsx
+++ src/components/layouts/ProductCard.jsx
@@ -2,11 +2,11 @@ import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 
 const ProductCard = ({ product }) => {
   return (
-    <Card className="pb-2 bg-white" isPressable onPress={() => console.log("item pressed")}>
+    <Card className="pb-2" isPressable onPress={() => console.log("item pressed")}>
       {/* <CardBody className="overflow-visible py-2"> */}
         <Image
           alt="Card background"
-          className="object-center rounded-xl mix-blend-soft-light bg-white"
+          className="object-center rounded-xl mix-blend-multiply bg-white"
           src={product?.imageURL}
           layout="fixed" // Ensures the image maintains its aspect ratio
           width={200} // Fixed width

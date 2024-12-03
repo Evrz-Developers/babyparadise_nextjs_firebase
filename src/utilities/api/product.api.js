a9f16227e0073a9c0492e12d6e3e081703efbc21
Robin K <dev.robinkurian@email.com>
2024-12-04 00:16:50 +0530

    update product fetching api add option for passing query parameter

--- src/utilities/api/product.api.js
+++ src/utilities/api/product.api.js
@@ -1,17 +1,22 @@
-const getProducts = async (category = null) => {
+const API_HOST = process.env.NEXT_PUBLIC_API_URL_PROD;
+
+const getProducts = async (category = null, req = null) => {
     try {
-      const url = new URL(`/api/products`);
-      if (category) url.searchParams.append("category", category); // Optionally filter by category
-  
-      const response = await fetch(url);
-      const data = await response.json();
-      return { data: data.data };
+        const baseUrl = req && req.headers ? `${req.headers.origin}` : API_HOST; 
+        const url = new URL(`${baseUrl}/api/products`);
+        if (category) url.searchParams.append("category", category); // Optionally filter by category
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return { data: data.data };
     } catch (error) {
         // TODO: Remove log
         console.error("Error fetching products:", error);
         throw error;
     }
-  };
+};
   
   const PRODUCT_API = {
     getProducts,

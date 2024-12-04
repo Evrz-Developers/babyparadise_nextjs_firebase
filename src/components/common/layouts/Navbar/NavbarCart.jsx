f88b1aa861dbeb227a774dc3d6b6f2d04499086c
Robin K <dev.robinkurian@email.com>
2024-12-04 16:10:39 +0530

    add link to cart

--- src/components/common/layouts/Navbar/NavbarCart.jsx
+++ src/components/common/layouts/Navbar/NavbarCart.jsx
@@ -1,14 +1,15 @@
 import { ShoppingCartIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const NavbarCart = () => {
   return (
-    <button className="relative">
+    <Link href="/cart" className="relative">
       <ShoppingCartIcon className="h-6 w-6 text-gray-700" />
       <span className="absolute -top-1 -right-2 bg-red-500 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
         3
       </span>
-    </button>
+    </Link>
   );
 };
 

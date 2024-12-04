ea649adde23671c31f88da168c7910f0501e089d
Robin K <dev.robinkurian@email.com>
2024-12-05 00:50:06 +0530

    fix contentWrapper for adding top-bottom padding

--- src/components/common/layouts/ContentWrapper.jsx
+++ src/components/common/layouts/ContentWrapper.jsx
@@ -1,10 +1,6 @@
 // CONTENTWRAPPER FOR WRAPPING THE CONTENTS WITH COMMON STYLING
 const ContentWrapper = ({ children, className = "" }) => {
-  return (
-    <div className={`container mx-auto ${className}`}>
-      {children}
-    </div>
-  );
+  return <div className={`container py-4 ${className}`}>{children}</div>;
 };
 
 export default ContentWrapper;

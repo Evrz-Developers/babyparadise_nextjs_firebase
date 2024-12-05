0a680fa2985769c98e86e1a12620021aeddf8e43
Robin K <dev.robinkurian@email.com>
2024-12-05 23:26:59 +0530

    Remove PushNotificationManager component from the Home page to streamline the layout and improve performance. This change simplifies the initial data structure passed to the Home component.

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,17 @@
 import Home from "@/components/shop/Home";
 import PRODUCT_API from "@/utilities/api/product.api";
 import FUNCTIONS from "@/utilities/functions";
-import PushNotificationManager from "@/app/PushNotificationManager";
 
 export default async function Page() {
   // PREFETCH DATA FOR HOME PAGE
   // PREFETCH DATA FOR HOME PAGE
   const { data: PRODUCTS } = await PRODUCT_API.getProducts(null);
   const CAROUSEL = await FUNCTIONS.CAROUSEL.GET_IMAGES();
-
   const INITIAL_DATA = { PRODUCTS, CAROUSEL };
 
   return (
     <div className="w-full">
       <Home initialData={INITIAL_DATA} />
-      <PushNotificationManager />
     </div>
   );
 }

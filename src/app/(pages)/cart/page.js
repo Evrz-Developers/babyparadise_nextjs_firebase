8996a87cc9d63aa8db88929c1b431b9140502a40
Robin K <dev.robinkurian@email.com>
2024-12-08 02:58:49 +0530

    feat: implement cart page with dynamic product fetching and loading state
    - Added CartPage component to manage cart functionality.
    - Integrated local storage for cart items and user authentication checks.
    - Implemented loading state while fetching cart data.
    - Utilized PageWrapper for consistent layout and breadcrumb navigation.
    - Set up dummy cart data for testing purposes.

--- src/app/(pages)/cart/page.js
+++ src/app/(pages)/cart/page.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import Cart from '@/components/shop/user/Cart';
+"use client";
+import React, { useEffect, useState } from "react";
+import Cart from "@/components/shop/user/Cart";
+import CART_API from "@/utilities/api/cart.api";
+import PageWrapper from "@/components/common/layouts/PageWrapper";
+import { dummyCartItems } from "@/components/shop/user/dummyCartData";
+export default function CartPage() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const cartPage = () => {
-    return (
-        <Cart />
-    )
+  useEffect(() => {
+    // TODO: Remove this after testing
+    const initializeDummyData = () => {
+      localStorage.setItem("cartItems", JSON.stringify(dummyCartItems));
+    };
+    initializeDummyData(); // Call to set up dummy data
+    const fetchCartData = async () => {
+      const user = JSON.parse(localStorage.getItem("user"));
+      const isLoggedIn = user ? true : false;
+
+      let fetchedProducts;
+      if (isLoggedIn) {
+        console.log("isLoggedIn");
+        const { data } = await CART_API.getProductsInCart();
+        fetchedProducts = data;
+      } else {
+        // Fetch cart items from local storage if not logged in
+        console.log("notLoggedIn");
+        fetchedProducts = JSON.parse(localStorage.getItem("cartItems")) || [];
+        console.log("cartItems", fetchedProducts);
+      }
+
+      setProducts(fetchedProducts);
+      setLoading(false);
+    };
+
+    fetchCartData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <PageWrapper breadcrumbItems={[{ label: "Cart", href: `#` }]}>
+      <Cart products={products} />
+    </PageWrapper>
+  );
 }
-
-export default cartPage

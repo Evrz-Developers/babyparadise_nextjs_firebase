97d7601793fa12224e39f4f0edba9827c0973759
Robin K <dev.robinkurian@email.com>
2024-12-08 22:54:38 +0530

    refactor: standardize string quotes and enhance CarouselItem component
    - Changed string quotes in CarouselItem component from single to double for consistency.
    - Added 'priority' attribute to the Image component to improve loading performance.
    - Ensured code formatting is uniform for better readability.

--- src/components/common/layouts/Carousel/CarouselItem.jsx
+++ src/components/common/layouts/Carousel/CarouselItem.jsx
@@ -1,19 +1,20 @@
 // CarouselItem
-import Image from 'next/image';
+import Image from "next/image";
 
 const CarouselItem = ({ src, alt, className, onClick }) => {
   return (
     <div
       className={`relative min-w-full ${className} flex items-center justify-center cursor-pointer`}
       onClick={onClick}
-      style={{ height: 'auto', aspectRatio: '851 / 315' }} // Maintain aspect ratio
+      style={{ height: "auto", aspectRatio: "851 / 315" }} // Maintain aspect ratio
     >
-      <Image 
-        src={src} 
-        alt={alt} 
-        fill 
-        style={{ objectFit: 'contain' }} 
-        className="rounded-md" 
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        style={{ objectFit: "contain" }}
+        className="rounded-md"
+        priority
       />
     </div>
   );

05a70622b8d0915ece0be982db6f7263c0755d89
Robin K <dev.robinkurian@email.com>
2024-12-08 22:13:01 +0530

    refactor: update carousel API endpoint for product fetching
    - Changed the API endpoint in carousel utility from `/api/carousel` to `/api/products` to align with the new product API structure.
    - Will be reverted

--- src/utilities/api/carousel.api.js
+++ src/utilities/api/carousel.api.js
@@ -2,7 +2,7 @@ const DEFAULT_HOST = process.env.NEXT_PUBLIC_API_URL_PROD;
 
 const getCarousel = async (host = DEFAULT_HOST) => {
     try {
-        const url = new URL(`${host}/api/carousel`);
+        const url = new URL(`${host}/api/products`);
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);

1d449c98e18a7eb746b1b70cd44f6d6b3666ac90
Robin K <dev.robinkurian@email.com>
2024-12-08 22:15:08 +0530

    refactor: update carousel API endpoint to improve data fetching
    - Changed the API endpoint in the carousel utility from `/api/products` to `/api/carousel` to align with the updated carousel functionality.
    - Enhanced error handling and logging for better debugging during data fetching.

--- src/utilities/api/carousel.api.js
+++ src/utilities/api/carousel.api.js
@@ -1,21 +1,21 @@
 const DEFAULT_HOST = process.env.NEXT_PUBLIC_API_URL_PROD;
 
 const getCarousel = async (host = DEFAULT_HOST) => {
-    try {
-        const url = new URL(`${host}/api/products`);
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        return { data: data.data };
-    } catch (error) {
-        // TODO: Remove log
-        console.error("Error fetching carousel:", error);
-        throw error;
+  try {
+    const url = new URL(`${host}/api/carousel`);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const data = await response.json();
+    return { data: data.data };
+  } catch (error) {
+    // TODO: Remove log
+    console.error("Error fetching carousel:", error);
+    throw error;
+  }
 };
-  
+
 const CAROUSEL_API = {
   getCarousel,
 };

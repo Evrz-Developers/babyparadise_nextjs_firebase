96c41a3effdea02ccbad5e95ecc8137f29788d89
Robin K <dev.robinkurian@email.com>
2024-12-11 20:52:29 +0530

    refactor: update Tailwind CSS configuration and global styles
    - Modified Tailwind CSS configuration to include additional NextUI components (form and input) for enhanced styling options.
    - Updated global CSS variables to adjust the primary foreground color for improved contrast and accessibility.
    
    These changes enhance the styling capabilities and visual consistency of the project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(breadcrumbs|button|card|dropdown|image|navbar|ripple|spinner|menu|divider|popover).js",
+    "./node_modules/@nextui-org/theme/dist/components/(breadcrumbs|button|card|divider|dropdown|form|image|input|navbar|ripple|spinner|menu|popover).js"
   ],
   theme: {
     extend: {

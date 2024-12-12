2733136fb75461646f4ed4eca60e60a6bd0623e6
Robin K <dev.robinkurian@email.com>
2024-12-12 16:48:55 +0530

    add dashboard component for admin

---
+++ src/app/(pages)/dashboard/page.js
@@ -0,0 +1,8 @@
+import Dashboard from "@/components/shop/admin/Dashboard";
+import React from "react";
+
+const DashboardPage = () => {
+  return <Dashboard />;
+};
+
+export default DashboardPage;
--- src/components/shop/admin/Dashboard.jsx
+++ src/components/shop/admin/Dashboard.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import DummyCard from '@/components/common/DummyCard';
+"use client";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import useCategoryStore from "@/store/useCategoryStore";
+import Product from "@/components/shop/product/Product";
 
 const Dashboard = () => {
-    return (
-            <DummyCard
-                gradientColor="#2634"
-                title="🚧 Dashboard"
-            />
-    )
-}
+  const { categories } = useCategoryStore();
+  const router = useRouter();
+  console.log("first", categories);
+  const handleClick = (categoryId) => {
+    router.push(`/category/${categoryId}`);
+  };
 
-export default Dashboard
+  return (
+    <div>
+      {categories && Array.isArray(categories) && categories.length > 0 ? (
+        <div className="h-full w-full xxs:mt-16 md:mt-14 fixed top-0">
+          <div className="w-full h-full pt-4">
+            <Product />
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-full pb-60">
+          <h2 className="text-xl font-semibold text-gray-800">
+            No Products to Show!
+          </h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;

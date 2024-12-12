23c0a10d47ee0c98d26283cfe902c8dca7a9b6eb
Robin K <dev.robinkurian@email.com>
2024-12-12 05:41:03 +0530

    Comment out with info and not using CustomDropdown now for login button hover

--- src/components/common/CustomDropdown.jsx
+++ src/components/common/CustomDropdown.jsx
@@ -5,13 +5,35 @@ import React from "react";
 INFO: Add a way to Open and close the dropdown based on mouse events and useState.
 
 EXAMPLE:
+import { LOGGED_IN_MENU_ITEMS } from "@/utilities/constants";
 const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
 <Button
 variant="light"
 onPress={onOpen}
 onMouseEnter={() => setIsDropdownOpen(true)}
 > 
 
+  const renderMenuItems = (items) => {
+    return items.map((item) => {
+      const Component = item.href ? Link : "button";
+      return (
+        <Component key={item.label} href={item?.href} onClick={item?.onClick}>
+          {item.label}
+        </Component>
+      );
+    });
+  };
+
+<CustomDropdown
+            isOpen={isDropdownOpen}
+            setIsDropdownOpen={setIsDropdownOpen}
+          >
+            <div className="flex flex-col gap-2">
+              {renderMenuItems(LOGGED_IN_MENU_ITEMS)}
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          </CustomDropdown>
 */
 
 const CustomDropdown = ({ isOpen, children, setIsDropdownOpen }) => {

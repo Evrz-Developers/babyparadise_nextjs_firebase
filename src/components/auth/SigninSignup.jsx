39de5b92b2b7fa494135e192b1fd835bac93911a
Robin K <dev.robinkurian@email.com>
2024-12-13 20:28:07 +0530

    refactor: enhance SigninSignup component with loading state management and integrate LoadingSpinner for improved user experience during Google login

--- src/components/auth/SigninSignup.jsx
+++ src/components/auth/SigninSignup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AUTH from "@/app/firebase/auth";
 import { toast } from "react-toastify";
 import { Form } from "@nextui-org/form";
@@ -12,9 +12,11 @@ import useLoggedUserStore from "@/store/useLoggedUserStore";
 import FUNCTIONS from "@/utilities/functions";
 import { FcGoogle } from "react-icons/fc";
 import BlurFade from "@/components/ui/blur-fade";
+import LoadingSpinner from "@/components/common/LoadingSpinner";
 
 export default function SigninSignup({ onClose }) {
   const [selected, setSelected] = React.useState("login");
+  const [isLoading, setIsLoading] = useState(false);
   const setUser = useLoggedUserStore((state) => state.setUser);
   const router = useRouter();
 
@@ -49,10 +51,13 @@ export default function SigninSignup({ onClose }) {
 
   const handleGoogleLogin = async () => {
     try {
+      setIsLoading(true);
       const result = await AUTH.SIGNIN_WITH_GOOGLE();
       FUNCTIONS.AUTH.HANDLE_LOGIN_RESULT(result, onClose, setUser, router);
     } catch (error) {
       toast.error("Google login failed");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -60,141 +65,145 @@ export default function SigninSignup({ onClose }) {
     <div className="flex flex-col w-full">
       <Card className="max-w-full w-[340px]s h-[400px]">
         <CardBody className="overflow-hidden">
-          <Tabs
-            fullWidth
-            aria-label="Tabs form"
-            selectedKey={selected}
-            variant="light"
-            radius="lg"
-            size="md"
-            onSelectionChange={setSelected}
-          >
-            <Tab key="login" title="Login">
-              <BlurFade key="login-form">
-                <Form
-                  className="flex flex-col gap-4"
-                  onSubmit={handleLoginWithEmail}
-                >
-                  <Input
-                    type="email"
-                    name="email"
-                    label="Email"
-                    placeholder="Enter your email"
-                    isRequired
-                  />
-                  <Input
-                    type="password"
-                    name="password"
-                    label="Password"
-                    placeholder="Enter your password"
-                    isRequired
-                  />
-                  <p className="text-center text-small">
-                    Need to create an account?{" "}
-                    <Link
-                      size="sm"
-                      onPress={() => setSelected("sign-up")}
-                      className="cursor-pointer"
-                    >
-                      Sign up
-                    </Link>
-                  </p>
-                  <div className="flex gap-2">
-                    <Button
-                      type="submit"
-                      color="primary"
-                      variant="shadow"
-                      fullWidth
-                      className="text-neutral-900 bg-color-primary-p80/15 hover:bg-color-primary-p60/15"
-                    >
-                      Login
-                    </Button>
-                    <div className="flex items-center gap-2">
-                      <div className="h-[1px] flex-1 bg-gray-200"></div>
-                      <p className="text-small text-gray-500">|</p>
-                      <div className="h-[1px] flex-1 bg-gray-200"></div>
+          {isLoading ? (
+            <LoadingSpinner />
+          ) : (
+            <Tabs
+              fullWidth
+              aria-label="Tabs form"
+              selectedKey={selected}
+              variant="light"
+              radius="lg"
+              size="md"
+              onSelectionChange={setSelected}
+            >
+              <Tab key="login" title="Login">
+                <BlurFade key="login-form">
+                  <Form
+                    className="flex flex-col gap-4"
+                    onSubmit={handleLoginWithEmail}
+                  >
+                    <Input
+                      type="email"
+                      name="email"
+                      label="Email"
+                      placeholder="Enter your email"
+                      isRequired
+                    />
+                    <Input
+                      type="password"
+                      name="password"
+                      label="Password"
+                      placeholder="Enter your password"
+                      isRequired
+                    />
+                    <p className="text-center text-small">
+                      Need to create an account?{" "}
+                      <Link
+                        size="sm"
+                        onPress={() => setSelected("sign-up")}
+                        className="cursor-pointer"
+                      >
+                        Sign up
+                      </Link>
+                    </p>
+                    <div className="flex gap-2">
+                      <Button
+                        type="submit"
+                        color="primary"
+                        variant="shadow"
+                        fullWidth
+                        className="text-neutral-900 bg-color-primary-p80/15 hover:bg-color-primary-p60/15"
+                      >
+                        Login
+                      </Button>
+                      <div className="flex items-center gap-2">
+                        <div className="h-[1px] flex-1 bg-gray-200"></div>
+                        <p className="text-small text-gray-500">|</p>
+                        <div className="h-[1px] flex-1 bg-gray-200"></div>
+                      </div>
+                      <Button
+                        isIconOnly
+                        color="primary"
+                        variant="shadow"
+                        fullWidth
+                        onPress={handleGoogleLogin}
+                        className="text-neutral-900 bg-color-primary-p80/15 hover:bg-color-primary-p60/15"
+                      >
+                        <FcGoogle className="text-xl" />
+                      </Button>
                     </div>
-                    <Button
-                      isIconOnly
-                      color="primary"
-                      variant="shadow"
-                      fullWidth
-                      onPress={handleGoogleLogin}
-                      className="text-neutral-900 bg-color-primary-p80/15 hover:bg-color-primary-p60/15"
-                    >
-                      <FcGoogle className="text-xl" />
-                    </Button>
-                  </div>
-                </Form>
-              </BlurFade>
-            </Tab>
-            <Tab key="sign-up" title="Sign up">
-              <BlurFade key="signup-form">
-                <Form
-                  className="flex flex-col gap-4 h-[300px]"
-                  onSubmit={handleRegisterWithEmail}
-                >
-                  <Input
-                    type="text"
-                    name="name"
-                    label="Name"
-                    placeholder="Enter your name"
-                    isRequired
-                  />
-                  <Input
-                    type="email"
-                    name="email"
-                    label="Email"
-                    placeholder="Enter your email"
-                    isRequired
-                  />
-                  <Input
-                    type="password"
-                    name="password"
-                    label="Password"
-                    placeholder="Enter your password"
-                    isRequired
-                  />
-                  <p className="text-center text-small">
-                    Already have an account?{" "}
-                    <Link
-                      size="sm"
-                      onPress={() => setSelected("login")}
-                      className="cursor-pointer"
-                    >
-                      Login
-                    </Link>
-                  </p>
-                  <div className="flex gap-2">
-                    <Button
-                      type="submit"
-                      color="primary"
-                      variant="shadow"
-                      fullWidth
-                      className="text-neutral-900 bg-color-primary-p80/15 hover:bg-color-primary-p60/15"
-                    >
-                      Sign up
-                    </Button>
-                    <div className="flex items-center gap-2">
-                      <div className="h-[1px] flex-1 bg-gray-200"></div>
-                      <p className="text-small text-gray-500">|</p>
-                      <div className="h-[1px] flex-1 bg-gray-200"></div>
+                  </Form>
+                </BlurFade>
+              </Tab>
+              <Tab key="sign-up" title="Sign up">
+                <BlurFade key="signup-form">
+                  <Form
+                    className="flex flex-col gap-4 h-[300px]"
+                    onSubmit={handleRegisterWithEmail}
+                  >
+                    <Input
+                      type="text"
+                      name="name"
+                      label="Name"
+                      placeholder="Enter your name"
+                      isRequired
+                    />
+                    <Input
+                      type="email"
+                      name="email"
+                      label="Email"
+                      placeholder="Enter your email"
+                      isRequired
+                    />
+                    <Input
+                      type="password"
+                      name="password"
+                      label="Password"
+                      placeholder="Enter your password"
+                      isRequired
+                    />
+                    <p className="text-center text-small">
+                      Already have an account?{" "}
+                      <Link
+                        size="sm"
+                        onPress={() => setSelected("login")}
+                        className="cursor-pointer"
+                      >
+                        Login
+                      </Link>
+                    </p>
+                    <div className="flex gap-2">
+                      <Button
+                        type="submit"
+                        color="primary"
+                        variant="shadow"
+                        fullWidth
+                        className="text-neutral-900 bg-color-primary-p80/15 hover:bg-color-primary-p60/15"
+                      >
+                        Sign up
+                      </Button>
+                      <div className="flex items-center gap-2">
+                        <div className="h-[1px] flex-1 bg-gray-200"></div>
+                        <p className="text-small text-gray-500">|</p>
+                        <div className="h-[1px] flex-1 bg-gray-200"></div>
+                      </div>
+                      <Button
+                        isIconOnly
+                        color="primary"
+                        variant="shadow"
+                        fullWidth
+                        onPress={handleGoogleLogin}
+                        className="text-neutral-900 bg-color-primary-p80/15 hover:bg-color-primary-p60/15"
+                      >
+                        <FcGoogle className="text-xl" />
+                      </Button>
                     </div>
-                    <Button
-                      isIconOnly
-                      color="primary"
-                      variant="shadow"
-                      fullWidth
-                      onPress={handleGoogleLogin}
-                      className="text-neutral-900 bg-color-primary-p80/15 hover:bg-color-primary-p60/15"
-                    >
-                      <FcGoogle className="text-xl" />
-                    </Button>
-                  </div>
-                </Form>
-              </BlurFade>
-            </Tab>
-          </Tabs>
+                  </Form>
+                </BlurFade>
+              </Tab>
+            </Tabs>
+          )}
         </CardBody>
       </Card>
     </div>

702cb41be66640c7f3dacf19aabcf38cd885da1c
Robin K <dev.robinkurian@email.com>
2024-12-14 06:19:25 +0530

    feat: add localStorage support to carousel and product stores for persistent state management

--- src/store/useCarouselStore.js
+++ src/store/useCarouselStore.js
@@ -2,7 +2,11 @@ import { create } from "zustand";
 
 const useCarouselStore = create((set) => ({
   carousel: [],
-  setCarousel: (carousel) => set({ carousel }),
+  setCarousel: (carousel) => {
+    localStorage.setItem("carousel", JSON.stringify(carousel));
+    set({ carousel });
+  },
+  getCarousel: () => JSON.parse(localStorage.getItem("carousel")) || [],
 }));
 
 export default useCarouselStore;

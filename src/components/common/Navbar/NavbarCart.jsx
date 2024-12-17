5f5ff67f121000e6f29106b87ae727ef328fa131
Robin K <dev.robinkurian@email.com>
2024-12-17 23:57:48 +0530

    feat: update NavbarCart component to display item count and handle cart opening
    - Integrated useCartStore to manage cart state and retrieve product count.
    - Replaced Link with onPress handler for improved interaction.
    - Added visual indication of item count in the cart icon.

--- src/components/common/Navbar/NavbarCart.jsx
+++ src/components/common/Navbar/NavbarCart.jsx
@@ -1,22 +1,26 @@
 import React from "react";
-import Link from "next/link";
 import { Button } from "@nextui-org/button";
 import { FaOpencart } from "react-icons/fa";
+import useCartStore from "@/store/useCartStore";
 
-const NavbarCart = () => {
+const NavbarCart = ({ onOpen }) => {
+  const { products } = useCartStore();
+  const itemCount = products.length;
+
   return (
     <Button
-      as={Link}
       variant="flat"
       className="bg-color-primary-p100/35 text-color-secondary-s30 hover:bg-color-primary-p90 hover:text-color-secondary-s05"
-      href="/cart"
+      onPress={onOpen}
     >
-      <div className="flex items-center cursor-pointer">
+      <div className="flex items-center cursor-pointer relative">
+        <FaOpencart className="h-5 w-5 mr-2" />
         <p className="font-medium">Cart</p>
-        <FaOpencart className="h-5 w-5 ml-1 " />
-        {/* <span className="absolute -top-1 -right-2 bg-red-500 text-white text-xs rounded-full w-3 h-3 flex items-center justify-center">
-        3
-      </span> */}
+        {itemCount > 0 && (
+          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+            {itemCount}
+          </span>
+        )}
       </div>
     </Button>
   );

7c0abb9c8d4d85c982d6f4434048fcd9f743e429
Robin-Kurian <dev.robinkurian@gmail.com>
2024-12-20 04:46:46 +0530

    feat: enhance EmptyCart component with login and product addition functionality
    - Updated EmptyCart component to conditionally render content based on user login status.
    - Integrated NextUI Button for improved user interaction, allowing logged-in users to add products and prompting non-logged-in users to log in.
    - Enhanced messaging to provide clearer guidance for users regarding cart actions.
    - This update improves user experience by streamlining the interaction flow within the shopping cart.

--- src/components/shop/user/EmptyCart.jsx
+++ src/components/shop/user/EmptyCart.jsx
@@ -1,8 +1,12 @@
 // EmptyCart.js
 import React from "react";
 import { FaOpencart } from "react-icons/fa6";
-import LinkAsButton from "@/components/common/LinkAsButton";
-const EmptyCart = ({ className }) => {
+import { Button } from "@nextui-org/button";
+import useDrawerStore from "@/store/useDrawerStore";
+
+const EmptyCart = ({ className = "", isLoggedIn = false }) => {
+  const { openLoginDrawer, onCartClose } = useDrawerStore();
+
   return (
     <div
       className={`flex flex-col items-center justify-center h-full ${className}`}
@@ -10,13 +14,32 @@ const EmptyCart = ({ className }) => {
       <div className="mb-4">
         <FaOpencart size={128} className="text-4xl text-color-primary-p40" />
       </div>
-      <h2 className="text-xl font-bold">Missing Cart items?</h2>
-      <p className="text-gray-600">
-        Login to see the items you added previously
-      </p>
-      <LinkAsButton href="/login" className="mt-4">
-        Login
-      </LinkAsButton>
+      <h2 className="text-xl font-bold mb-2">Oops! Missing Cart items?</h2>
+      {isLoggedIn ? (
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-gray-600 text-xs mb-2">
+            Add products to your cart.
+          </p>
+          <Button
+            onPress={onCartClose}
+            className="mt-4 bg-color-primary-p60 text-white"
+          >
+            Add Products
+          </Button>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-gray-600 text-xs mb-2">
+            Login to proceed with checkout.
+          </p>
+          <Button
+            onPress={openLoginDrawer}
+            className="mt-4 bg-color-primary-p60 text-white"
+          >
+            Login
+          </Button>
+        </div>
+      )}
     </div>
   );
 };

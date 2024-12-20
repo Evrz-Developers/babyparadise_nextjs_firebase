2644c13e9f43b800c968d703b1bc89150b99cf84
Robin-Kurian <dev.robinkurian@gmail.com>
2024-12-20 11:07:38 +0530

    fix: adjust layout of NotFound component for improved visual consistency
    - Updated the NotFound component to change the container class from 'min-h-screen' to 'min-h-full' and added vertical margin for better spacing.
    - This change enhances the overall layout and user experience when a 404 error occurs, ensuring the content is centered and visually appealing.

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,7 +4,7 @@ import { MdHome } from "react-icons/md";
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+    <div className="min-h-full flex flex-col items-center justify-center my-20">
       <div className="text-center space-y-4">
         <h1 className="text-4xl font-bold text-primary">404</h1>
         <h2 className="text-2xl font-semibold text-foreground">

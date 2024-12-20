5a9e6ed5987213b9ce4fe8526bd2417c1bfd3733
Robin-Kurian <dev.robinkurian@gmail.com>
2024-12-20 11:07:49 +0530

    fix: update Error component layout for improved spacing and visual consistency
    - Changed the container class from 'min-h-screen' to 'min-h-full' and added vertical margin to the Error component.
    - This adjustment enhances the overall layout, ensuring better centering and spacing of the error message for a more user-friendly experience.

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Error({ error, reset }) {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+    <div className="min-h-full flex flex-col items-center justify-center my-20">
       <div className="text-center space-y-4">
         <h1 className="text-4xl font-bold text-danger">Oops!</h1>
         <h2 className="text-2xl font-semibold text-foreground">

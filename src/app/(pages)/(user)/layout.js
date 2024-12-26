2eb362c37e2df7f07d1f8477780c583f1ff38fe2
Robin-Kurian <dev.robinkurian@gmail.com>
2024-12-26 23:09:50 +0530

    refactor: simplify layout components by removing unused session handling
    - Removed commented-out session handling and AdminLayout from RootLayout for cleaner code.
    - Streamlined UserLayout by eliminating session data access, focusing on layout structure.
    - These changes enhance readability and maintainability of layout components.

--- src/app/(pages)/(user)/layout.js
+++ src/app/(pages)/(user)/layout.js
@@ -1,11 +1,8 @@
-import { useSession } from "next-auth/react";
 
 export default function UserLayout({ children }) {
-  const { data: session } = useSession();
 
   return (
     <div className="user-layout">
-      {/* You can now access session data here */}
       {children}
     </div>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,9 @@
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/system";
 import DefaultLayout from "@/components/common/layouts/DefaultLayout";
-import AdminLayout from "@/app/admin/layout";
 import { ToastContainer } from "react-toastify";
 import { Manrope, Oswald } from "next/font/google";
 import "react-toastify/dist/ReactToastify.css";
-// import { getSession } from "next-auth/react";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -23,18 +21,11 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
-  // const session = await getSession();
-  // const isAdmin = session?.user?.role === "admin";
-
   return (
     <html lang="en">
       <body className={`${manrope.variable} antialiased`}>
         <NextUIProvider>
-          {/* {isAdmin ? (
-            <AdminLayout>{children}</AdminLayout>
-          ) : ( */}
           <DefaultLayout>{children}</DefaultLayout>
-          {/* )} */}
           <ToastContainer
             position="bottom-right"
             autoClose={1500}
